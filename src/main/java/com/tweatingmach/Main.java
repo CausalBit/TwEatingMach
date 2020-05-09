cddda6b0fc7173e04592f42f95e47c7fe1b1641a
alonzazo <oscar.azofeifa.segura@gmail.com>
2020-05-08 18:01:40 -0600

    x

--- src/main/java/com/tweatingmach/Main.java
+++ src/main/java/com/tweatingmach/Main.java
@@ -2,20 +2,22 @@
 package com.tweatingmach;
 
 
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.Accumulator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.function.*;
-import org.apache.spark.storage.StorageLevel;
 import org.apache.spark.streaming.*;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
-import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.api.java.*;
 
-import org.apache.spark.streaming.receiver.Receiver;
+
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import scala.Tuple2;
 
 import java.util.*;
@@ -28,59 +30,28 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        List<String> filters = new ArrayList<String>();
-        filters.add("game of thrones");
-        filters.add("Game of Thrones");
-        filters.add("#GoT");
-        filters.add("#winterishere");
-        filters.add("#winteriscoming");
-        filters.add("#gameofthrones");
-
-       /*
-        TwitterSource twitterSource = new TwitterSource();
-
-        try {
-            twitterSource.streamData(filters, null);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-
-        try{
-            Thread.sleep(10000);
-        }
-        catch(InterruptedException ex)
-        {
-            Thread.currentThread().interrupt();
-        }*/
 
         SparkConf conf = new SparkConf().setAppName("TwEatingMachine").setMaster("local[4]");
-        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(10)); //set batch interval to 10s
-        /*JavaReceiverInputDStream<String> stream = jssc.receiverStream(new JavaTwitterCustomReceiver(filters,null));*/
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1)); //set batch interval to 10s
 
-        /*JavaRDD<String> textos = jssc.sparkContext().parallelize(Arrays.asList("hola", "como"));
+        String topics = "kafka-chat";
+        String brokers = "localhost:9092";
+        String groupId = "kafka-sandbox";
+        Set<String> topicsSet = new HashSet<>(Arrays.asList(topics.split(",")));
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        kafkaParams.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        kafkaParams.put(ConsumerConfig.CLIENT_ID_CONFIG, "main");
+        kafkaParams.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        kafkaParams.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        kafkaParams.put("auto.offset.reset", "earliest");
+        kafkaParams.put("enable.auto.commit", false);
 
-        PriorityQueue<JavaRDD<String>> queue = new PriorityQueue<>();
-        queue.add(textos);
-        JavaDStream<String> stream = jssc.queueStream(queue);
-*/
-        JavaReceiverInputDStream<String> stream = jssc.receiverStream(new Receiver<String>(StorageLevel.MEMORY_ONLY()) {
-
-
-            @Override
-            public void onStop() {
-
-            }
-
-            @Override
-            public void onStart() {
-                Random random = new Random();
-                String[] words = new String[]{"{\"text\": \"The worst thing is you\"}", "{\"text\": \"I love you\"}", "{\"text\": \"es lo mejor de la vida\"}"};
-                for (int i = 0; i < 100; i++){
-                    Integer salt = random.nextInt(2);
-                    store(words[(i + salt) % words.length]);
-                }
-            }
-        });
+        // Create direct kafka stream with brokers and topics
+        JavaInputDStream<ConsumerRecord<String, String>> stream = KafkaUtils.createDirectStream(
+                jssc,
+                LocationStrategies.PreferConsistent(),
+                ConsumerStrategies.Subscribe(topicsSet, kafkaParams));
 
         ArrayList<Accumulator<Integer>> acc = new ArrayList<>();
         for (int i = 0; i < 5; i++)
@@ -89,98 +60,12 @@ public class Main {
 
         //Parse the text into objects.
         JavaDStream<JSONObject> tweetObjects = stream.map(tweet -> {
-            JSONObject tweetObject;
-            try {
-                tweetObject = (JSONObject) new JSONParser().parse(tweet);
-                //System.out.println("The tweet: "++""+tweetObject.get("text"));
-            } catch (Exception e) {
-                tweetObject = new JSONObject();
-            }
+            JSONObject tweetObject = new JSONObject();
+            tweetObject.<String,String>put("text", tweet.value());
             return tweetObject;
         });
 
-
-        /*
-        	Users can amplify the broadcast of Tweets authored by other users by retweeting .
-        	Retweets can be distinguished from typical Tweets by the existence of a
-        	retweeted_status attribute. This attribute contains a representation of the original
-        	Tweet that was retweeted. Note that retweets of retweets do not show representations
-        	of the intermediary retweet, but only the original Tweet. (Users can also unretweet
-        	a retweet they created by deleting their retweet.)
-        */
-
-        //Take only the non retweets.
-        JavaDStream<JSONObject> originalTweets = tweetObjects.filter(tweet -> {
-            if (tweet.containsKey("retweeted_status")) {
-                //System.out.println("Retweeted!");
-                return false;
-            } else {
-                //System.out.println("I am not retweeted");
-                return true;
-            }
-        });
-                //Take only original in English
-                /*.filter(tweet -> {
-                    if (tweet.get("lang") == null) {
-                        return false;
-                    } else if (tweet.get("lang").equals("en")) {
-                        System.out.println("English detected");
-                        return true;
-                    } else {
-                        System.out.println("I am not in english");
-                        return false;
-                    }
-                })*/
-                //Take only original tweets with geolocation.
-                /*.filter(tweet -> {
-                    if (tweet.get("place") == null) {
-                        System.out.println("The place is null ");
-                        return false;
-                    } else {
-                        JSONObject tweetObject;
-                        try {
-                            tweetObject = (JSONObject) new JSONParser().parse(tweet.get("place").toString());
-                            System.out.println("Place name: " + tweetObject.get("full_name"));
-                        } catch (Exception e) {
-                            System.out.println("Exception " + e.getMessage());
-                        }
-                        return true;
-                    }
-                });*/
-
-
-        //Take only original in spanish
-        JavaDStream<JSONObject> oriSpanishTweets = originalTweets.filter(tweet -> {
-            try {
-                if (tweet.get("lang") == null) {
-                    return false;
-                } else if (tweet.get("lang").equals("es")) {
-                    System.out.println("Spanish detected ");
-                    return true;
-                } else {
-                    //System.out.println("I am not in Spanish");
-                    return false;
-                }
-            } catch (NullPointerException e) {
-                System.out.println("Exception " + e.getMessage());
-                return false;
-            }
-        })
-
-
-                //Take only original tweets with geolocation.
-                .filter(tweet -> {
-                    if (tweet.get("coordinates") == null) {
-                        //System.out.println("I have no coordinates: "+tweet.get("coordinates"));
-                        return false;
-                    } else {
-                        System.out.println("Coordinates: " + tweet.get("coordinates"));
-                        return true;
-                    }
-                });
-
-
-        JavaPairDStream<Integer, Integer> tweetsSentiments = originalTweets.mapToPair(tweet -> {
+        JavaPairDStream<Integer, Integer> tweetsSentiments = tweetObjects.mapToPair(tweet -> {
             String the_tweet = tweet.get("text").toString();
             if (the_tweet == null) {
                 System.out.println("No tweet");
